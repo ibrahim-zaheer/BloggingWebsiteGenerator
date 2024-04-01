c48d938944162b487f97799fb3d2e08d0291ed75
Ibrahim-Za <fa21-bcs-009@cuilahore.edu.pk>
2024-04-01 22:39:25 +0500

    not much changes

--- texttoHtmlApp/urls.py
+++ texttoHtmlApp/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.convertor_view, name='convertor_view'),
+    path('generate_html/', views.generate_html, name='generate_html'),
 ]
--- texttoHtmlApp/views.py
+++ texttoHtmlApp/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
 from .forms import MyForm
+from django.http import HttpResponse
 
 def convertor_view(request):
     form = MyForm()
     return render(request, 'index.html', {'form': form})
+
+
+def generate_html(request):
+    if request.method == 'POST':
+        html_content = request.POST.get('html_content', '')
+
+        # Ensure there is content to generate HTML file
+        if html_content:
+            response = HttpResponse(html_content, content_type='text/html')
+            response['Content-Disposition'] = 'attachment; filename="generated_html.html"'
+            return response
+        else:
+            return HttpResponse("No HTML content provided", status=400)
+    else:
+        return HttpResponse("Method not allowed", status=405)
